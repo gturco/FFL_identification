f2ab9a0dfa7a091ac72f9684ee3d2ef6e82d3058
Gina <gturco@Owners-Air.att.net>
2017-01-03 15:56:46 -0800

    clean up code added vascular data for overlap

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ Downstream targets of VND7, MYB46 FFLs were identified by comparing overlap betw
 
 ## Results
 
-12 shared, 392 VND7 (with 12 shared genes still included) and 281 MYB46  (with 12 shared genes still included)
-
+12 shared, 392 VND7 (with 12 shared genes still included) and 281 MYB46  (with 12 shared genes still included) <br />
+recently added vascular genes but these are missin the cesa8 and need cesa4 and lac11 with other ath gene see miguels data for this
 
 ## Datasheets
 
 [GEO List](https://docs.google.com/spreadsheets/d/1bBGAHmDUdcbXpEYZKhLlAjDdungymd6gtOfFQtjtfI4/edit#gid=0) <br /> 
 [MYB46 DEX induction paper used](http://onlinelibrary.wiley.com/doi/10.1111/j.1365-313X.2009.03989.x/full) <br /> 
-[VND7 protoplast est induction paper used](http://www.cell.com/molecular-plant/pdf/S1674-2052(14\)60556-6.pdf) <br /> 
+[Shows MYB46 & MYB83 directly regulate CESA4,CESA8 and CES7, also good link for comparing binding motifs](http://onlinelibrary.wiley.com/doi/10.1111/j.1365-313x.2012.05124.x/full)
+[VND7 protoplast est induction paper + cylo used](https://www.ncbi.nlm.nih.gov/pubmed/20935069) <br />
+[VND7 GR/DEX + cylo](https://www.ncbi.nlm.nih.gov/pubmed/21284754)
--- src/micro_ffl.py
+++ src/micro_ffl.py
@@ -39,6 +39,7 @@ def readfile_vnd(filename):
                 logfc = float(args[-6])
                 pval = float(args[-2])
                 if pval >= 0.05: continue
+                #if logfc < 0 : continue
                 d[gene_name].append((logfc,pval))
             except ValueError: continue
             genes.append(gene_name)
@@ -75,34 +76,41 @@ def readfile_vnda(filename):
     return d
 
 
-def find_shared_genes(vnd7,myb46):
+def read_vas(vas_file):
+    genes = []
+    for line in open(vas_file):
+        genes.append(line.strip())
+    print line
+    return  set(genes)
+
+
+def find_shared_genes(vnd7,myb46,vas):
     vnd7_genes = set(vnd7.keys())
     myb46_genes = set(myb46.keys())
-    shared = list(vnd7_genes & myb46_genes)
-    x = 0
-    for gene in shared:
-        logfc=  vnd7[gene][0][0]
-        print gene
-        if logfc > 0:
-            x += 1
-            print x
-
-    return list(shared), len(vnd7_genes), len(myb46_genes)
+    shared = vnd7_genes.intersection(myb46_genes)
+    vnd7_vas = vnd7_genes.intersection(vas)
+    myb46_vas = vas.intersection(myb46_genes)
+    all_shared = shared.intersection(vas)
     
-def main(vnd7_microarray, myb46_microarray):
+    print "AT4G18770" in vnd7_genes
+    print len(vas), len(vnd7_genes), len(myb46_genes)
+    print len(shared), len(myb46_vas), len(vnd7_vas), len(all_shared)
+    print all_shared
+
+def main(vnd7_microarray, myb46_microarray, vas_genes):
     vnd7 = readfile_vnd(vnd7_microarray)
     #myb46 = readfile_myb(myb46_microarray)
     myb46 = readfile_mybsig(myb46_microarray)
-    find_shared_genes(vnd7, myb46)
-    shared_genes, vnd7_n, myb46_n = find_shared_genes(vnd7, myb46)
-    print len(shared_genes), vnd7_n, myb46_n
+    vas = read_vas(vas_genes)
+    find_shared_genes(vnd7, myb46, vas)
+    #print len(shared_genes), vnd7_n, myb46_n
     #print shared_genes
 
 
 #main("GSE23555.csv", "myb46_ramires.txt")
 #main("GSE23555.csv", "GSE16143.csv")
 #main("GSE23555.csv", "GSE25838.csv")
-main("../data/GSE23555.csv", "../data/TPJ_3989_sm_Table S1.csv")
+main("../data/GSE23555.csv", "../data/TPJ_3989_sm_Table S1.csv","../data/At_Vas_genes.txt")
 
 
 ### the vnd7 dataset has half pos half neg
